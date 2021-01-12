28c5b976ecd88e459bff174b7358430d3d0291e9
yuriDuque <yurithielmann83@gmail.com>
2021-01-11 22:26:57 -0300

    colocando o saldo como privado

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     depositar(valor) {
         if (valor > 0)
-            conta1.saldo += valor;
+            conta1._saldo += valor;
         else
             console.log('O valor não pode ser depositado indisponivel!')
     }
 
     sacar(valor) {
-        if (conta1.saldo >= valor)
-            conta1.saldo -= valor;
+        if (conta1._saldo >= valor)
+            conta1._saldo -= valor;
         else
             console.log('Saldo indisponivel!')
     }
@@ -27,7 +27,6 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 const conta1 = new ContaCorrente();
-conta1.saldo = 0;
 conta1.agencia = 1001;
 
 conta1.depositar(100);
@@ -41,7 +40,6 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const conta1 = new ContaCorrente();
-conta1.saldo = 0;
 conta1.agencia = 1001;
 
 
